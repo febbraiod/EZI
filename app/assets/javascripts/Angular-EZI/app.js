e0289309e73ef1f4f70fa92b7d2768582dda105a
febbraiod <donedesign1@gmail.com>
2016-05-22 14:10:22 -0400

    add states (routes) to app module

--- app/assets/javascripts/Angular-EZI/app.js
+++ app/assets/javascripts/Angular-EZI/app.js
@@ -1,2 +1,60 @@
 angular
-  .module('app', ['ui.router', 'ngResource']);
+  .module('app', ['ui.router', 'ngResource'])
+  .config(function($stateProvider){
+    $stateProvider
+      .state('welcome', {
+        url: '/welcome',
+        templateUrl: '#',
+        controller: 'LoginController'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: '#',
+        controller: 'RegistrationController'
+      })
+      .state('vehicles', {
+        url: '/vehicles',
+        templateUrl: '#',
+        controller: 'VehiclesController'
+      })
+      .state('vehicles.single', {
+        url: '/vehicles/:vin',
+        templateUrl: '#',
+        controller: 'VehiclesController'
+      })
+      .state('vehicles.delivered', {
+        url: '/vehicles/delivered',
+        templateUrl: '#',
+        controller: 'VehiclesController'
+      })
+      .state('user', {
+        url: '/users/:name',
+        templateUrl: '#',
+        controller: 'VehiclesController'
+      })
+      .state('admin', {
+        url: '/admin',
+        templateUrl: '#',
+        controller: 'AdminController'
+      })
+      .state('admin.users', {
+        url: '/users',
+        templateUrl: '#',
+        controller: 'UsersController'
+      })
+      .state('admin.dealership', {
+        url: '/analytics',
+        templateUrl: '#',
+        controller: 'StatsController'
+      })
+      .state('admin.import', {
+        url: '/vehicleimport',
+        templateUrl: '#',
+        controller: 'ImportController'
+      })
+      .state('admin.swap', {
+        url: '/swaplist',
+        templateUrl: '#',
+        controller: 'SwapController'
+      });
+  });
