a5a6939ee26888eabd6e845524d1931e309dd236
febbraiod <donedesign1@gmail.com>
2016-05-21 20:13:33 -0400

    Added angular and set up landing page

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,14 +2,16 @@ class Vehicle < ActiveRecord::Base
   validates :VIN, uniqueness: true
 
   def self.import(file, new_or_used)
+    # CSV.parse(file.tempfile.read.gsub ",http://", ', http://')
     CSV.foreach(file.tempfile, headers: true, :header_converters => lambda { |h| h.try(:downcase).tr(" ", "_") }) do |row|
       vehicle_hash = row.to_hash
       vehicle_hash["new_or_used"] = new_or_used
       mappings = {"stock_#" => "stock_no", "colour" => "color", "drivetrain_desc" => "drive_train", "vin" => "VIN"}
       vehicle_hash.keys.each { |k| vehicle_hash[ mappings[k] ] = vehicle_hash.delete(k) if mappings[k] }
+      vehicle_hash["photo_url_list"] = vehicle_hash["photo_url_list"].split(",")
       Vehicle.create(vehicle_hash.except("dealerid", "dealer_name", "dealer_address", "dealer_city", 
-      "dealer_region", "dealer_postal_code", "dealer_phone_number", "description", "series", "third_party_feature_codes", "video_url_duration",
-      "video_url", "video_source_"))
+      "dealer_region", "dealer_postal_code", "dealer_phone_number", "description", "placeholder1","placeholder2","series", "third_party_feature_codes", "video_url_duration",
+      "video_url", "video_source_", "video_source"))
     end
   end
 
