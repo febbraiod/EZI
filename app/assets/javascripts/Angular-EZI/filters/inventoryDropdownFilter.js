4e0c0c55ce1c433608c680bf6985e0518f13aba1
febbraiod <donedesign1@gmail.com>
2016-05-26 16:46:09 -0400

    fix filter to include vista roof

--- app/assets/javascripts/Angular-EZI/filters/inventoryDropdownFilter.js
+++ app/assets/javascripts/Angular-EZI/filters/inventoryDropdownFilter.js
@@ -11,7 +11,7 @@ function inventoryDropdownFilter(){
         if(dropdownvalue === 'New' || dropdownvalue === 'Used'){
           return vehicle.new_or_used === dropdownvalue;
         }else if(dropdownvalue === 'moonroof'){
-          return vehicle.options.indexOf("moonroof") > -1 || vehicle.options.indexOf("Moonroof") > -1 || vehicle.options.indexOf("sunroof") > -1 || vehicle.options.indexOf("Sunroof") > -1
+          return vehicle.options.indexOf("moonroof") > -1 || vehicle.options.indexOf("Moonroof") > -1 || vehicle.options.indexOf("sunroof") > -1 || vehicle.options.indexOf("Sunroof") > -1 ||  vehicle.options.indexOf("Vista Roof");
         }else{
           return true;
         }
