dba964fb7be4427dec1e738f111acc700fb9efbb
febbraiod <donedesign1@gmail.com>
2016-05-26 17:53:52 -0400

    flesh out CRUD on rails side for vehicles

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -12,6 +12,33 @@ module Api
         render json: @vehicle
       end
 
+      def create 
+        @vehicle = Vehicle.new(vehicle_params) 
+        if @vehicle.save 
+          render json: @vehicle
+        end 
+      end
+
+      def update 
+        @vehicle = Note.find_by(id: params[:id])
+        if @vehicle.update(vehicle_params) 
+          render json: @vehicle
+        end 
+      end
+
+
+      private
+
+      def vehicle_params
+        params.require(:vehicle).permit(:stock_no, :year, :make, :model, :VIN, 
+                                        :odometer, :msrp, :dealer_price, :color,
+                                        :interior_color, :photo_url_list, :engine,
+                                        :drive_train, :fuel, :body_type, :options,
+                                        :transmission, :new_or_used, :storage, 
+                                        :delivered)
+      end
+
+
     end
   end
 end
