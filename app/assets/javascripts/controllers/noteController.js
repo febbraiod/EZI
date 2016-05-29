216687c2526ad6b5d6a2e04cb6a0906fa18e851f
febbraiod <donedesign1@gmail.com>
2016-05-29 11:52:59 -0400

    add NoteController

--- app/assets/javascripts/controllers/noteController.js
+++ app/assets/javascripts/controllers/noteController.js
@@ -1,6 +1,17 @@
-function NoteController() {
-  var ctrl= this;
+function NoteController($stateParams, noteService) {
+  var ctrl = this;
   
+  ctrl.note = new Note();
+  ctrl.vehicle_id = $stateParams.id;
+  // ctrl.user = $scope.user;
+
+  ctrl.addNote = function() {
+    ctrl.note.vehicle_id = ctrl.vehicle_id;
+    debugger
+    ctrl.note.$save(function(resp) {
+      debugger
+    });
+  };
 
 }
 
