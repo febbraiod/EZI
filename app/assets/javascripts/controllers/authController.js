fd9b4282c695a593f2698ac0a896233d3532d712
febbraiod <donedesign1@gmail.com>
2016-05-30 10:10:22 -0400

    fixed controller names in prep to refactor role handling

---
+++ app/assets/javascripts/controllers/authController.js
@@ -0,0 +1,27 @@
+function AuthController($scope, $state, Auth){
+
+  var config = {
+      headers: {
+          'X-HTTP-Method-Override': 'POST'
+      }
+  };
+
+  // scope works but using 'this' didn't why?
+  $scope.login = function() {
+    Auth.login($scope.user, config).then(function(){
+      $state.go('inventory');
+    });
+  };
+
+  // getting 422 unprossable entity..
+  // thinking about over writing Devise's Registration controller
+  $scope.register = function() {
+    Auth.register($scope.user).then(function(){
+      $state.go('inventory');
+    });
+  };
+}
+
+angular
+  .module('app')
+  .controller('AuthController', AuthController);
