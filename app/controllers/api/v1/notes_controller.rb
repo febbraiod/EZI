31e1d7b48e50f1121eefde3f846697053366a7c9
febbraiod <donedesign1@gmail.com>
2016-05-30 12:12:44 -0400

    commit for merge

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,28 +2,20 @@ module Api
   module V1
     class NotesController < ApplicationController
 
-      def index
-        binding.pry
-        @notes = Note.where(vehicle_id: (params['vehicle_id'].to_i))
-        render json: @notes
-      end
+      # not sure if i need this index action, but i don't want to delete it in case i get an error.
+      # I think rendering groups of notes is handled entirely through the vehicle
 
-      def show
-        @note = Note.find_by(id: params[:id])
-        render json: @note
-      end
+      # def index
+      #   binding.pry
+      #   @notes = Note.where(vehicle_id: (params['vehicle_id'].to_i))
+      #   render json: @notes
+      # end
+
 
       def create
         @note = Note.new(note_params)
         @note.user = current_user
         if @note.save 
-          render json: @note
-        end 
-      end
-
-      def update 
-        @note = Note.find_by(id: params[:id])
-        if @note.update(note_params) 
           render json: @note
         end 
       end
